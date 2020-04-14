04d8260e7511010785daa617785407b8ffc1e979
Joshua Brule <joshua.brule@jhuapl.edu>
2020-04-14 18:57:25 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lantern",
+    version="0.1.0",
+    author="Joshua Brule",
+    author_email="joshua.brule@jhuapl.edu",
+    description="Tools to encode PyTorch modules as Z3 constraints",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.jhuapl.edu/brulejt1/lantern",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    python_requires=">=3"
+    install_requires=["torch",
+                      "z3-solver"]
+)
